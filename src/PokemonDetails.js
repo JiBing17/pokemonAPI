24cf9173a458c1c938f0f9d4f10caa5c23ecd000
Ji Bing Ni <jibingni17@gmail.com>
2024-05-12 14:16:11 -0400

    added pokemon types to card

--- src/PokemonDetails.js
+++ src/PokemonDetails.js
@@ -25,7 +25,7 @@ function PokemonDetails() {
         const evolutionChainResponse = await axios.get(evolutionChainUrl);
         setEvolutionChain(parseEvolutionChain(evolutionChainResponse.data));
 
-        // retrieves the names of the top four moves from the fetched data
+        // Retrieve the names of the top four moves from the fetched data
         const movesUrl = response.data.moves.slice(0, 4).map(move => move.move.name);
         setMoves(movesUrl);
 
@@ -43,48 +43,78 @@ function PokemonDetails() {
   const parseEvolutionChain = (chain) => {
     const stages = [];
     let currentStage = chain.chain;
-    // Traverse the evolution chain and store object with name / pic
-    while (currentStage) {
-      console.log(currentStage.species.url)
 
+    // Traverse the evolution chain and store objects with name and sprite
+    while (currentStage) {
       stages.push({
         name: currentStage.species.name,
-        // gets number id of current pokemon for png 
         sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${currentStage.species.url.split("/")[6]}.png`,
       });
-
       currentStage = currentStage.evolves_to[0];
     }
 
     return stages;
   };
+
   // Render loading message if data is still loading
   if (loading) {
     return <div>Loading...</div>;
   }
+
   // Render error message if there's an error
   if (error) {
     return <div>Error: {error.message}</div>;
   }
+
   // Render not found message if no pokemon details were found
   if (!pokemonDetails) {
     return <div>Pokemon details not found.</div>;
   }
 
-  // destructure pokemon detail object
-  const { name, sprites, stats } = pokemonDetails;
+  // Destructure pokemon detail object
+  const { name, sprites, stats, types } = pokemonDetails;
+
+  // Define background colors based on Pokemon types
+  const typeColors = {
+    normal: "#A8A878",
+    fighting: "#C03028",
+    flying: "#A890F0",
+    poison: "#A040A0",
+    ground: "#E0C068",
+    rock: "#B8A038",
+    bug: "#A8B820",
+    ghost: "#705898",
+    steel: "#B8B8D0",
+    fire: "#F08030",
+    water: "#6890F0",
+    grass: "#78C850",
+    electric: "#F8D030",
+    psychic: "#F85888",
+    ice: "#98D8D8",
+    dragon: "#7038F8",
+    dark: "#705848",
+    fairy: "#EE99AC",
+  };
 
   return (
     <div className="container mx-auto px-4 pt-20">
       <div className="flex justify-center">
         {/* Display Pokemon's Name and Picture */}
         <div className="bg-gray-200 p-8 rounded-lg mb-6 flex flex-col items-center w-96">
-          <h2 className="text-2xl font-bold text-center mb-4">{name.toUpperCase()}</h2>
+          <h2 className="text-2xl font-bold mb-2">{name.toUpperCase()}</h2>
+          {/* Display Pokemon's Types */}
+          <div className="flex">
+            {types.map((type, index) => (
+              <span key={index} className={`text-white px-4 py-1 rounded-lg mx-2`} style={{ backgroundColor: typeColors[type.type.name] }}>
+                {type.type.name}
+              </span>
+            ))}
+          </div>
           {sprites && (
             <img
               src={sprites.front_default}
               alt={`Image of ${name}`}
-              className="w-48 h-48 object-contain mb-4"
+              className="w-48 h-48 object-contain my-4"
             />
           )}
           <div className="mt-6 text-center">
@@ -95,7 +125,7 @@ function PokemonDetails() {
           </div>
         </div>
       </div>
-      {/* Display evolutions */}
+      {/* Display evolutions*/}
       <div className="mt-8">
         <h2 className="text-2xl font-bold mb-4">{name.toUpperCase()}'S Evolutions</h2>
         <div className="flex justify-center">
@@ -123,5 +153,3 @@ function PokemonDetails() {
 }
 
 export default PokemonDetails;
-
-
