ba5372b751cad9fc5ed3c29c08df8b158f6f8c80
Ji Bing Ni <jibingni17@gmail.com>
2024-05-15 12:01:20 -0400

    fixed home link to work on any page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import PokemonDetails from "./PokemonDetails";
 import Home from "./Home";
 
 function App() {
+
+  // Function used to navigate to the home page (page 1) 
+  const handleHeaderClick = () => {
+    window.location.href = "/"; 
+  };
+
   return (
     <Router>
       <div className="container mx-auto px-4">
         <div className="fixed top-0 left-0 w-full bg-white z-10 shadow-md py-4 px-4">
-          <Link to="/" className="text-4xl font-bold py-4 text-center md:text-left md:ml-4 md:py-0 md:w-auto md:mr-auto">PokeAPI Data</Link>
+          { /* When the header link is clicked, navigate to the home page */ }
+          <Link to="/" className="text-4xl font-bold py-4 text-center md:text-left md:ml-4 md:py-0 md:w-auto md:mr-auto" onClick={handleHeaderClick}>
+            PokeAPI Data
+          </Link>
         </div>
       </div>
       <Routes>
