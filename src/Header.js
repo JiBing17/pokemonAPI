b9a178849cef2623582df082c02297aa78346f9c
Ji Bing Ni <jibingni17@gmail.com>
2024-05-25 18:12:38 -0400

    replaced text link with icon link for favs and logout

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, Box, IconButton } from '@mui/material';
 import { useAuth } from './AuthContext';
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
 function Header() {
     // Get the setIsAuthenticated function from the authentication context
@@ -20,18 +22,21 @@ function Header() {
     return (
         <AppBar position="fixed" style={{ background: '#C22E28' }}>  
             <Toolbar>
+                {/** Home Link **/}
                 <Box component={Link} to="/" sx={{ display: 'flex', alignItems: 'center', textDecoration: 'none', color: 'inherit', flexGrow: 1 }}>
                     <img
                         src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pokémon_logo.svg"
                         style={{ height: '40px'}}
                     />
                 </Box>
-                <Box component={Link} to="/pokemon/favorites" sx={{ textDecoration: 'none', color: 'inherit', mr: 2 }}>
-                    Favorites
-                </Box>
-                <Button color="inherit" onClick={handleLogout}>
-                    Logout
-                </Button>
+                {/** Favorite Cards Link **/}
+                <IconButton component={Link} to="/pokemon/favorites" color="inherit" sx={{ mr: 2 }}>
+                    <FavoriteIcon />
+                </IconButton>
+                {/** Logout Link **/}
+                <IconButton color="inherit" onClick={handleLogout}>
+                    <ExitToAppIcon /> 
+                </IconButton>
                 
             </Toolbar>
         </AppBar>
