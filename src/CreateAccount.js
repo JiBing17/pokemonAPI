52f295f83f1b196a41514050c4bf3444fa9a307a
Ji Bing Ni <jibingni17@gmail.com>
2024-05-31 19:01:33 -0400

    added gradientbackground to login / createAccount with styles added

--- src/CreateAccount.js
+++ src/CreateAccount.js
@@ -7,121 +7,136 @@ function CreateAccount() {
     // State hooks for handling form data and error messages
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [error, setError] = useState('');  
+    const [error, setError] = useState('');
 
     const navigate = useNavigate(); // Tool used for navigation across routes (Used to go to Login Route after new account is created)
 
     // Function handles registration form submission
     const handleRegister = async () => {
-
         // Check if username or password is empty
         if (!username || !password) {
             setError('Both fields are required.');
-            return;  
+            return;
         }
 
         try {
             // Makes an asynchronous POST request to the register endpoint of the backend API
             await axios.post('http://localhost:5000/api/users/register', { username, password });
             alert('Account created successfully!');
-            navigate('/login');  // Takes user back to login Page to login the newly created account
-
+            navigate('/login'); // Takes user back to login Page to login the newly created account
         } catch (err) {
             setError(err.response.data || 'Failed to create account.'); // Sets error if any that will display at bottom of form
-
         }
     };
 
     // Create Account Form
     return (
-        <Container maxWidth="sm">
-            <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center', boxShadow: "0px 2px 4px rgba(0,0,0,0.5)"}}>
-                <Paper sx={{ padding: 4, width: '100%', mt: 2 }}>
-                    {/** Create Account Title **/}
-                    <Typography component="h1" variant="h5" textAlign="center">
-                        Create Account
-                    </Typography>
-                    {/** Create Username Field **/}
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        label="Username"
-                        autoFocus
-                        value={username}
-                        onChange={e => setUsername(e.target.value)}
-                        sx={{
-                            mt: 2,
-                            '& .MuiOutlinedInput-root': {
-                              '& fieldset': {
-                                borderColor: '#C22E28',
-                              },
-                              '&:hover fieldset': {
-                                borderColor: '#C22E28',
-                              },
-                              '&.Mui-focused fieldset': {
-                                borderColor: '#C22E28',
-                              },
-                            },
-                            '& .MuiInputLabel-root': {
-                              color: 'black',
-                            },
-                            '& .MuiInputLabel-root.Mui-focused': {
-                              color: '#black',
-                            }
-                          }}
-                    />
-                    {/** Create Password Field **/}
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        label="Password"
-                        type="password"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                        sx={{
-                            mt: 2,
-                            '& .MuiOutlinedInput-root': {
-                              '& fieldset': {
-                                borderColor: '#C22E28',
-                              },
-                              '&:hover fieldset': {
-                                borderColor: '#C22E28',
-                              },
-                              '&.Mui-focused fieldset': {
-                                borderColor: '#C22E28',
-                              },
-                            },
-                            '& .MuiInputLabel-root': {
-                              color: 'black',
-                            },
-                            '& .MuiInputLabel-root.Mui-focused': {
-                              color: '#black',
-                            }
-                          }}
-                    />
-                    {/** Submit to Create New User Button **/}
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        onClick={handleRegister}
-                        sx={{ mt: 3, mb: 2, backgroundColor: "#C22E28", '&:hover': { backgroundColor: '#B22222'}}}
-                    >
-                        Sign Up
-                    </Button>
-                    {/** Switch to Create Account Form Link**/}
-                    {error && <Typography color="error" textAlign="center">{error}</Typography>}
-                    <Link to="/login" style={{ textDecoration: 'none' }}>
-                        <Button variant="text" sx={{color: "#C22E28"}}>Already have an account? Log In</Button>
-                    </Link>
-                </Paper>
-            </Box>
-        </Container>
+        <div style={{ minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)' }}>
+            {/* Background Gradient */}
+            <Container maxWidth="sm" sx={{
+                display: 'flex',
+                flexDirection: 'column'
+            }}>
+                <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center', boxShadow: "0px 2px 4px rgba(0,0,0,0.5)" }}>
+                    <Paper sx={{ padding: 4, width: '100%' }}>
+                        {/** Create Account Title **/}
+                        <Typography component="h1" variant="h5" textAlign="center">
+                            Create Account
+                        </Typography>
+                        {/** Create Username Field **/}
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            label="Username"
+                            autoFocus
+                            value={username}
+                            onChange={e => setUsername(e.target.value)}
+                            sx={{
+                                mt: 2,
+                                '& .MuiOutlinedInput-root': { 
+                                    
+                                    // Username text field border
+                                    '& fieldset': {
+                                        borderColor: '#C22E28', 
+                                    },
+                                    // Username text field border (hover)
+                                    '&:hover fieldset': {
+                                        borderColor: '#C22E28',
+                                    },
+                                    // Username text field border (selected)
+                                    '&.Mui-focused fieldset': {
+                                        borderColor: '#C22E28',
+                                    },
+                                },
+                                // Username placeholder text 
+                                '& .MuiInputLabel-root': {
+                                  color: 'black',
+                                },
+                                // Username placeholder text (selected)
+                                '& .MuiInputLabel-root.Mui-focused': {
+                                  color: 'black',
+                                }
+                              }}
+                        />
+                        {/** Create Password Field **/}
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            label="Password"
+                            type="password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                            sx={{
+                                mt: 2,
+                                '& .MuiOutlinedInput-root': {
+
+                                    // Password text field border                                
+                                    '& fieldset': {
+                                        borderColor: '#C22E28',
+                                    },
+                                    // Password text field border (hover)                                
+                                    '&:hover fieldset': {
+                                        borderColor: '#C22E28',
+                                    },
+                                    // Password text field border (selected)                            
+                                    '&.Mui-focused fieldset': {
+                                        borderColor: '#C22E28',
+                                    },
+                                },
+                                // Password placeholder text 
+                                '& .MuiInputLabel-root': {
+                                  color: 'black',
+                                },
+                                // Password placeholder text (selected)
+                                '& .MuiInputLabel-root.Mui-focused': {
+                                  color: 'black',
+                                }
+                              }}
+                        />
+                        {/** Submit to Create New User Button **/}
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            onClick={handleRegister}
+                            sx={{ mt: 3, mb: 2, backgroundColor: "#C22E28", '&:hover': { backgroundColor: '#B22222'}}}
+                        >
+                            Sign Up
+                        </Button>
+                        {/** Switch to Create Account Form Link**/}
+                        {error && <Typography color="error" textAlign="center">{error}</Typography>}
+                        <Link to="/login" style={{ textDecoration: 'none' }}>
+                            <Button variant="text" sx={{color: "#C22E28"}}>Already have an account? Log In</Button>
+                        </Link>
+                    </Paper>
+                </Box>
+            </Container>
+        </div>
     );
 }
 
--- src/Login.js
+++ src/Login.js
@@ -9,114 +9,131 @@ function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const { setIsAuthenticated } = useAuth(); // Function used for authenticating users when login is successfull
-    const navigate = useNavigate(); // Runction used to navigate different routes (Homes.js if login succeeds)
+    const navigate = useNavigate(); // Function used to navigate different routes (Homes.js if login succeeds)
 
     const handleLogin = async () => {
         try {
             // Makes an asynchronous POST request to the login endpoint of the backend API
             const response = await axios.post('http://localhost:5000/api/users/login', { username, password });
 
-            // Successfull request, set user as authenticated and redirect to homes page
+            // Successful request, set user as authenticated and redirect to homes page
             if (response.status === 200) {
                 setIsAuthenticated(true);
                 navigate('/');
             }
-            // Sends alert due to unsuccessfull request for login
+            // Sends alert due to unsuccessful request for login
         } catch (error) {
             alert('Login failed! ' + (error.response?.data?.message || ''));
             console.error('Login error:', error);
         }
     };
+
     // Login Form
     return (
-        <Container maxWidth="sm">
-            <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center', boxShadow: "0px 2px 4px rgba(0,0,0,0.5)" }}>
-                <Paper sx={{ padding: 4, width: '100%', mt: 2 }}>
-                    {/** Login Title **/}
-                    <Typography component="h1" variant="h5" textAlign="center">
-                        Login
-                    </Typography>
-                    {/** Username Field **/}
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        label="Username"
-                        autoFocus
-                        value={username}
-                        onChange={e => setUsername(e.target.value)}
-                        sx={{
-                            mt: 2,
-                            '& .MuiOutlinedInput-root': {
-                              '& fieldset': {
-                                borderColor: '#C22E28',
-                              },
-                              '&:hover fieldset': {
-                                borderColor: '#C22E28',
-                              },
-                              '&.Mui-focused fieldset': {
-                                borderColor: '#C22E28',
-                              },
-                            },
-                            '& .MuiInputLabel-root': {
-                              color: 'black',
-                            },
-                            '& .MuiInputLabel-root.Mui-focused': {
-                              color: 'black',
-                            }
-                          }}
-                    />
-                    {/** Password Field **/}
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        label="Password"
-                        type="password"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                        sx={{
-                            mt: 2,
-                            '& .MuiOutlinedInput-root': {
-                              '& fieldset': {
-                                borderColor: '#C22E28',
-                              },
-                              '&:hover fieldset': {
-                                borderColor: '#C22E28',
-                              },
-                              '&.Mui-focused fieldset': {
-                                borderColor: '#C22E28',
-                              },
-                            },
-                            '& .MuiInputLabel-root': {
-                              color: 'black',
-                            },
-                            '& .MuiInputLabel-root.Mui-focused': {
-                              color: '#black',
-                            }
-                          }}
-                    />
-                    {/** Submit Login Button **/}
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        onClick={handleLogin}
-                        sx={{ mt: 3, mb: 2, backgroundColor: "#C22E28", '&:hover': { backgroundColor: '#B22222'}}}
-                    >
-                        Login
-                    </Button>
-                    {/** Switch to Create Account Form Link **/}
-                    <Link to="/register" style={{ textDecoration: 'none'}}>
-                        <Button variant="text" sx={{color: "#C22E28"}}>Don't have an account? Sign Up</Button>
-                    </Link>
-                </Paper>
-            </Box>
-        </Container>
+        <div style={{ minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)' }}>
+            {/* Background Gradient */}
+            <Container maxWidth="sm" sx={{ display: 'flex', flexDirection: 'column' }}>
+                <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center', boxShadow: "0px 2px 4px rgba(0,0,0,0.5)" }}>
+                    <Paper sx={{ padding: 4, width: '100%' }}>
+                        {/** Login Title **/}
+                        <Typography component="h1" variant="h5" textAlign="center">
+                            Login
+                        </Typography>
+                        {/** Username Field **/}
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            label="Username"
+                            autoFocus
+                            value={username}
+                            onChange={e => setUsername(e.target.value)}
+                            sx={{
+                                mt: 2,
+                                '& .MuiOutlinedInput-root': {
+
+                                    // Username text field border
+                                    '& fieldset': {
+                                        borderColor: '#C22E28',
+                                    },
+                                    // Username text field border (hover)
+                                    '&:hover fieldset': {
+                                        borderColor: '#C22E28',
+                                    },
+                                    // Username text field border (selected)
+                                    '&.Mui-focused fieldset': {
+                                        borderColor: '#C22E28',
+                                    },
+                                },
+                                // Username placeholder text 
+                                '& .MuiInputLabel-root': {
+                                  color: 'black',
+                                },
+                                // Username placeholder text (selected)
+                                '& .MuiInputLabel-root.Mui-focused': {
+                                  color: 'black',
+                                }
+                              }}
+                        />
+                        {/** Password Field **/}
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            label="Password"
+                            type="password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                            sx={{
+                                mt: 2,
+                                '& .MuiOutlinedInput-root': {
+
+                                    // Password text field border                                
+                                    '& fieldset': {
+                                        borderColor: '#C22E28',
+                                    },
+                                    // Password text field border (hover)                                
+                                    '&:hover fieldset': {
+                                        borderColor: '#C22E28',
+                                    },
+                                    // Password text field border (selected)                                
+                                    '&.Mui-focused fieldset': {
+                                        borderColor: '#C22E28',
+                                    },
+                                },
+                                // Password placeholder text 
+                                '& .MuiInputLabel-root': {
+                                  color: 'black',
+                                },
+                                // Password placeholder text (selected)
+                                '& .MuiInputLabel-root.Mui-focused': {
+                                  color: 'black',
+                                }
+                              }}
+                        />
+                        {/** Submit Login Button **/}
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            onClick={handleLogin}
+                            sx={{ mt: 3, mb: 2, backgroundColor: "#C22E28", '&:hover': { backgroundColor: '#B22222'}}}
+                        >
+                            Login
+                        </Button>
+                        {/** Switch to Create Account Form Link **/}
+                        <Link to="/register" style={{ textDecoration: 'none'}}>
+                            <Button variant="text" sx={{color: "#C22E28"}}>Don't have an account? Sign Up</Button>
+                        </Link>
+                    </Paper>
+                </Box>
+            </Container>
+        </div>
     );
 }
 
 export default Login;
+
